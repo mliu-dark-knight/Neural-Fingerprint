2a67e7a3c0aaa195f401b51a9be58f8b13478974
Mengxiong <mliu60@illinois.edu>
2017-09-20 16:40:04 -0500

    working

--- model.py
+++ model.py
@@ -1,33 +1,31 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 class NeuralFingerprint(nn.Module):
-	def __init__(self):
-		super(NeuralFingerprint, self).__init__()
-
 	def __init__(self, paras):
-		self.__init__()
+		super(NeuralFingerprint, self).__init__()
 		self.paras = paras
 		self.embedding = nn.Embedding(paras.num_feat, paras.F)
 		self.hidden = {i: nn.Linear(paras.F, paras.F) for i in range(paras.R)}
 		self.output_fp = {i: nn.Linear(paras.F, paras.L) for i in range(paras.R)}
-		self.output_cl = nn.Linear(paras.L, paras.num_label)
+		self.output_cl = nn.Linear(paras.L, paras.num_class)
 
 	def forward(self, graph):
-		f = torch.FloatTensor(self.paras.L).zero_()
-		embedding = F.torch.unsqueeze(self.embedding(graph.node_feature), 0)
+		f = Variable(torch.FloatTensor(self.paras.L).zero_(), requires_grad=True)
+		embedding = F.torch.unsqueeze(self.embedding(Variable(torch.from_numpy(graph.node_feature))), 1)
 		for L in range(self.paras.R):
 			tmp_embedding = []
 			# graph.nodes must be a sorted list
 			for node in graph.nodes:
 				v = embedding[node]
-				for neighbour in graph.neighbours(v):
-					v += embedding[neighbour]
+				for neighbour in graph.neighbours[node]:
+					v = torch.add(v, embedding[neighbour])
 				r = F.relu(self.hidden[L](v))
 				tmp_embedding.append(r)
 				i = F.softmax(self.output_fp[L](r))
-				f += i
+				f = torch.add(f, i)
 			for i, r in enumerate(tmp_embedding):
 				embedding[i] = r
-		return F.softmax(self.output_fp(f))
+		return F.softmax(self.output_cl(f))
--- predictor.py
+++ predictor.py
@@ -1,25 +1,73 @@
+import pickle
+import torch
+import numpy as np
 import torch.nn.functional as F
 import torch.optim as optim
 from tqdm import tqdm
 from model import NeuralFingerprint
 from torch.autograd import Variable
 
+class Graph(object):
+	def __init__(self, graph, label, feature_map):
+		self.label = np.asarray([label], dtype=int)
+		self.nodes = np.asarray([-1] * len(graph), dtype=int)
+		self.node_feature = np.asarray([-1] * len(graph), dtype=int)
+		self.neighbours = np.array([None] * len(graph))
+		self.init(graph, feature_map)
+
+	def init(self, graph, feature_map):
+		for node, info in graph.items():
+			self.nodes[node] = node
+			self.neighbours[node] = np.asarray(info['neighbors'], dtype=int)
+			self.node_feature[node] = feature_map[info['label']]
+
+
 class Predictor(object):
 	def __init__(self, paras):
 		self.paras = paras
-		self.train_data = None
-		self.test_data = None
-		self.model = NeuralFingerprint()
-		pass
+		self.data = self.load_data()
+		self.model = NeuralFingerprint(paras)
+
+	def map_feat_class(self):
+		feature_map = {}
+		class_map = {}
+		file = 'classification-datasets/' + self.paras.dataset + '/' + self.paras.dataset + '.graph'
+		with open(file) as f:
+			graphs = pickle.loads(f.read())
+		num_feature = 0
+		for _, graph in graphs['graph'].items():
+			for _, info in graph.items():
+				label = info['label']
+				assert len(label) == 1
+				if label not in feature_map:
+					feature_map[label] = num_feature
+					num_feature += 1
+		num_class = 0
+		for label in graphs['labels']:
+			if label not in class_map:
+				class_map[label] = num_class
+				num_class += 1
+		return feature_map, class_map
+
+	def load_data(self):
+		data = []
+		file = 'classification-datasets/' + self.paras.dataset + '/' + self.paras.dataset + '.graph'
+		feature_map, class_map = self.map_feat_class()
+		self.paras.num_feat, self.paras.num_class = len(feature_map), len(class_map)
+		with open(file) as f:
+			graphs = pickle.loads(f.read())
+		for id, graph in graphs['graph'].items():
+			data.append(Graph(graph, class_map[graphs['labels'][id]], feature_map))
+		return data
 
 	def train(self):
 		optimizer = optim.Adam(self.model.parameters(), lr=self.paras.learning_rate)
 		self.model.train()
-		for epoch in tqdm(range(self.paras.epoch), ncols=100):
-			for data, target in self.train_data:
+		for _ in tqdm(range(self.paras.epoch), ncols=100):
+			for i in tqdm(range(len(self.data)), ncols=100):
+				graph = self.data[i]
 				optimizer.zero_grad()
-				data, target = Variable(data), Variable(target)
-				output = self.model(data)
-				loss = F.cross_entropy(output, target)
+				output = self.model(graph)
+				loss = F.cross_entropy(output, Variable(torch.from_numpy(graph.label)))
 				loss.backward()
 				optimizer.step()
